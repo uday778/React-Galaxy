e8001a9eeb93faa5871cd36154f99b975ee54000
uday kumar <uday33379@gmail.com>
2024-03-13 12:13:49 +0530

    custom react added

--- 01-basic-react/src/main.jsx
+++ 01-basic-react/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'http://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+// const anotherElement=(
+//     <a href='https://google.com' target='_blank' >visit google</a>
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    // anotherElement
+    <App/>
  
 )
